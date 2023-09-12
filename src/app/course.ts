9f0c37403f029babe76ea0b404980c3d029c0c3c
ian otieno <ianotieno23@gmail.com>
2023-09-12 21:52:59 +0300

    update

--- src/app/course.ts
+++ src/app/course.ts
@@ -1,9 +1,16 @@
 export class Course{
     id:number
     name:string
-    constructor(id:number,name:string){
+    duration:string
+    tuition :string
+    mode:string
+
+    constructor(id:number,name:string,duration:string,tuition:string,mode:string){
         this.id=id,
-        this.name=name
+        this.name=name,
+        this.duration=duration
+        this.tuition=tuition
+        this.mode=mode
 
     }
 }
--- src/app/mock-courses.ts
+++ src/app/mock-courses.ts
@@ -1,11 +1,11 @@
 import { Course } from './course';
 
 export const COURSE:Course[]=[
-    {id:1, name :'C++'},
-    {id:2, name :'Python'},
-    {id:3, name :'Angular'},
-    {id:4, name :'Java'},
-    {id:5, name :'Devops'},
-    {id:6, name :'CyberSecurity'},
-    {id:7, name :'Ml'},
+    {id:1, name :'C++', duration:'two weeks', tuition:"200k",mode:"In-person & Online | Mon to Fri from 8am – 5pm"},
+    {id:2, name :'Python',duration:'one months', tuition:"100k",mode:"In-person & Online | Mon to Fri from 8am – 5pm"},
+    {id:3, name :'Angular',duration:'two months', tuition:"200k",mode:"In-person & Online | Mon to Fri from 8am – 5pm"},
+    {id:4, name :'Java',duration:'three weeks', tuition:"50k",mode:"In-person & Online | Mon to Fri from 8am – 5pm"},
+    {id:5, name :'Devops',duration:'one weeks', tuition:"350k",mode:"In-person & Online | Mon to Fri from 8am – 5pm"},
+    {id:6, name :'CyberSecurity',duration:'five weeks', tuition:"200k",mode:"In-person & Online | Mon to Fri from 8am – 5pm"},
+    {id:7, name :'Ml',duration:'one year', tuition:"500k",mode:"In-person & Online | Mon to Fri from 8am – 5pm"},
 ] 
