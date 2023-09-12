62fb6779e99cd2a59a62a33757b1d846b37d2127
ian otieno <ianotieno23@gmail.com>
2023-09-12 14:32:09 +0300

    dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CoursesComponent } from './courses/courses.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CourseDetailComponent } from './course-detail/course-detail.component';
 
 
-const routes: Routes = [{path: 'courses', component:CoursesComponent}];
+const routes: Routes = [
+  {path: '', redirectTo: './dashboard',pathMatch: 'full'},
+  {path: 'courses', component:CoursesComponent},
+  {path:'dashboard', component:DashboardComponent},
+  {path:'courses',component:CourseDetailComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { Course } from '../course';
+import { COURSE } from '../mock-courses';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  courses: Course[] = COURSE
 
 }
