9f0520e4ce23f66c4d319402daf2bb660c905804
ian otieno <ianotieno23@gmail.com>
2023-09-12 13:51:14 +0300

    message component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
 import { CourseDetailComponent } from './course-detail/course-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
-    CourseDetailComponent
+    CourseDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
